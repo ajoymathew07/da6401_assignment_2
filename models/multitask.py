b57539ae6674a52bc894cdffd2e156019679bb2c
Ajoy <ryukendo@Ajoys-MacBook-Air.local>
2026-04-08 19:36:27 +0530

    Updated id

--- models/multitask.py
+++ models/multitask.py
@@ -116,11 +116,11 @@ class MultiTaskPerceptionModel(nn.Module):
         """
         import gdown
 #         https://drive.google.com/file/d/1aD-PFsrIDWMqFMd8QOBzuCEhQ1w4HN-9/view?usp=sharing
+# https://drive.google.com/file/d/1JOgh_s-dyc01ZZjcfx7vfumqWvmtDWJc/view?usp=sharing
 
-# https://drive.google.com/file/d/1z5HxRX3Y4Ik3Qb6ryqj-9Z6RIviiKLol/view?usp=sharing
 # https://drive.google.com/file/d/1B4UKyuN5i-KOg8o3tB4BZCtwhuNCs5pe/view?usp=sharing
         gdown.download(id="1aD-PFsrIDWMqFMd8QOBzuCEhQ1w4HN-9", output=classifier_path, quiet=False)
-        gdown.download(id="1z5HxRX3Y4Ik3Qb6ryqj-9Z6RIviiKLol", output=localizer_path, quiet=False)
+        gdown.download(id="1JOgh_s-dyc01ZZjcfx7vfumqWvmtDWJc", output=localizer_path, quiet=False)
         gdown.download(id="1B4UKyuN5i-KOg8o3tB4BZCtwhuNCs5pe", output=unet_path, quiet=False)
 
         super().__init__()
